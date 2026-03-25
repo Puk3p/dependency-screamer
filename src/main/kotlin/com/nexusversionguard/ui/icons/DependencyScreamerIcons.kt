99f52026f7ecb45dec62af29eff7b5c6b55faecd
George Lupu <117625145+Puk3p@users.noreply.github.com>
2026-03-25 20:54:01 +0200

    chore: modify logo

---
+++ src/main/kotlin/com/nexusversionguard/ui/icons/DependencyScreamerIcons.kt
@@ -0,0 +1,8 @@
+package com.nexusversionguard.ui.icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object DependencyScreamerIcons {
+    val ToolWindow: Icon = IconLoader.getIcon("/icons/logo.svg", DependencyScreamerIcons::class.java)
+}
